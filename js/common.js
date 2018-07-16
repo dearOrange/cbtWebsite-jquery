c225cc4adc1fa1c5cc2d47a90ac3df779049e9b1
ccc123456 <pengjun1216@163.com>
2018-07-16 17:18:35 +0800

    车捕头官网

---
+++ js/common.js
@@ -0,0 +1,42 @@
+$(function(){
+
+　  $('#sectionSween').fullpage({
+        anchors: ['pageOne','pageTwo','pageThree','pageFour','pageFive','pageSix'],
+        menu: '#menu',
+    });   //激活fullpage的效果，可以检查页面看效果了
+
+
+    $('.wechat_hover').mouseenter(function(){
+        $('.wechat_img').css('display','block')
+    })
+    $('.wechat_hover').mouseleave(function(){
+        $('.wechat_img').css('display','')
+    })
+    $('.upAPP_hover').mouseenter(function(){
+        $('.app_loader').css('display','block')
+    })
+    $('.upAPP_hover').mouseleave(function(){
+        $('.app_loader').css('display','')
+    })
+   
+    $.ajax({
+        type: "get",
+        url: "http://hunter.chebutou.com.cn/hunterServer/content/getWebsiteData",
+        dataType: "json",
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        success: function(data){
+            var res = data.data;
+            if(res.code === 'SUCCESS'){
+                $('.third_number_task').html(res.data.totalTask);
+                $('.third_number_prices').html(res.data.totalPrices);
+                $('.third_number_downstream').html(res.data.totalDownstream);
+                $('.third_number_upstream').html(res.data.totalUpstream);
+            }
+        }
+    });
+});
+
+
+
